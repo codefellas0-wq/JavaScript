c9393e13bed93d15051fc976303eaeb0021fa66c
codefellas0-wq <codefellas0@gmail.com>
2026-04-24 17:38:47 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,18 @@
+let score = 123
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+//  "33" -> 33
+// "33abc" -> NaN
+let isloggedin = "n"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+//1 -> true
+//"" -> false
+//"naresh" -> true
+//0 -> false
+let num = 33
+let stringnum = String(num);
+console.log(stringnum)
+console.log(typeof(stringnum))
