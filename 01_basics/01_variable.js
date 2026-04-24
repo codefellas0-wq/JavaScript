e06791b35108d87f49c0bd3a6e481f8e47115906
codefellas0-wq <codefellas0@gmail.com>
2026-04-24 13:42:10 +0000

    01_variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountID = 1332
+let accountemail = "np5824@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaipur"
+let accountstate
+// accountID= 2;
+console.log(accountID)
+accountemail = "naresh"
+accountpassword = "123321"
+accountcity = "bhopal";
+console.table([accountID , accountemail,accountpassword,accountcity , accountstate])
