591e4409fc968077a1658e240b46cf7c685063d5
codefellas0-wq <codefellas0@gmail.com>
2026-04-24 14:01:24 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict";// treat all the js code as newer version 
+//alert("hello") we are using node js not browse
+let name = "naresh"
+let age = 18
+let isloggedin =true
+let state  = null
+// number => 2 to power 53
+//bigint 
+// string 
+// boolean 
+// null
+//undefined =>
+    //symbol => unique 
+//object - user defined datatype
+console.log(typeof age);
+console.log(typeof state) // object
+console.table([name , age , state , isloggedin])
